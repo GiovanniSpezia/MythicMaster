5acfd3846323be83e50fddba6f4d0a7668236a41
GioSpezia <102656560+Gioxox@users.noreply.github.com>
2025-01-12 11:35:08 +0100

    up

--- js/script.js
+++ js/script.js
@@ -2,11 +2,7 @@ document.querySelector('.discover-button').addEventListener('click', function() 
     window.location.href = '#chi-siamo';
 });
 
-
-window.addEventListener('load', () => {
-    const preloader = document.getElementById('preloader');
-    
-    setTimeout(() => {
-        preloader.classList.add('hidden'); // Nasconde il preloader
-    }, 2000);
-});
+function toggleMenu() {
+    const mobileMenu = document.querySelector('.mobile-menu');
+    mobileMenu.classList.toggle('active');
+}
